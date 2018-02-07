577a3215644c85ebefc79a20198f894d5043e006
xfn-bg-charge <395449240@qq.com>
2018-02-07 14:27:19 +0800

    数组知识

--- arrUniq.js
+++ arrUniq.js
@@ -11,3 +11,20 @@ export default function arrUniq(arr) {
     }
     return res
 }
+
+
+// js两个数组比较，找到相同的删除掉, obj里面的对象id只要在ary里面，就删除掉
+// var ary = [1,2,3,4];
+// var obj=[{
+//     id:1,
+//     name:'luo1'
+// },{
+//     id:2,
+//     name:'luo2'
+// },{
+//     id:5,
+//     name:'luo5'
+// }]
+//
+// //方法一
+// const newArr = obj.filter(item => !ary.includes(item.id))
